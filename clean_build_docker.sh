0728e05eae6f129ab8932ab88d6d17c90bb6040e
Vyacheslav Voronenko <git@voronenko.info>
2017-02-13 15:04:23 +0200

    docker adjustments

---
+++ clean_build_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+cd /opt/sphinxproject && make --makefile Makefiledck
--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-docker create -it -v $PWD:/opt/sphinxproject --name projectdocs softasap/sphinx:latest bash
+DOCKER_UID=$(id -u)
 
-# Now start it.
-docker start projectdocs
+DOCKER_USER=$(whoami)
 
-docker exec -it projectdocs "cd /opt/sphinxproject && make --makefile Makefiledck"
+RUNNING=$(docker inspect --format="{{ .State.Running }}" projectdocs 2> /dev/null)
+
+if [ $? -eq 1 ]; then
+  echo "PROJECTDOCS CONTAINER DOES NOT EXIST - CREATE IT"
+  docker create -it -v $PWD:/opt/sphinxproject --name projectdocs softasap/sphinx:latest bash
+  docker start projectdocs
+  echo projectdocs container created
+fi
+
+if [ "$RUNNING" != "true" ]; then
+  echo "RUN PROJECTDOCS CONTAINER"
+  docker run projectdocs
+  echo projectdocs container running
+fi
+
+echo "building docs"
+echo docker exec -it projectdocs bash -c "export DOCKER_UID=${DOCKER_UID} && export DOCKER_USER=${DOCKER_USER} && /opt/sphinxproject/docker_entry_point.sh"
+docker exec -it projectdocs bash -c "export DOCKER_UID=${DOCKER_UID} && export DOCKER_USER=${DOCKER_USER} && /opt/sphinxproject/docker_entry_point.sh"
